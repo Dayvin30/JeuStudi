2eb6345f989e5c083b8eaee61bdcdd726cc4a3fd
Dayvin30 <dayvin30@gmail.com>
2022-05-11 15:41:22 +0200

    ajout des fichiers

---
+++ index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Jeu du dé</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body class="grid">
+
+  <div id="p1-redblock" class="red-block"></div>
+  <div id="p2-redblock" class="red-block"></div>
+  <div id="txtPlayer1" class="txtPlayer1">Player 1</div>
+  <div id="txtPlayer2" class="txtPlayer2">Player 2</div>
+  <div id="current-p1" class="current-p1">CURRENT</div>
+  <div id="current-p2" class="current-p2">CURRENT</div>
+  <div id="dice-pic" class="dice-pic">
+
+  <img id="dice"
+     src="assets/dice_6.png">
+
+  </div>
+
+  <div onclick="newGame()" id="new-game" class="new-game"><i class="bi bi-plus-circle-fill"></i> NEW GAME</div>
+  <div onclick="RollDice()" id="roll-dice" class="roll-dice"><i class="bi bi-arrow-clockwise"></i> ROLL DICE</div>
+  <div onclick="Hold()" id="hold" class="hold"><i class="bi bi-box-arrow-in-down"></i> HOLD</div>
+  <div id="number-p1" class="number-p1">0</div>
+  <div id="number-p2" class="number-p2">0</div>
+  <div id="score-p1" class="score-p1">0</div>
+  <div id="score-p2" class="score-p2">0</div>
+
+
+
+</body>
+</html>
+
+
+<script>
+
+player="p1";
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function newGame(){
+
+  player="p1";
+
+  alert('Le joueur 1 commence !');
+
+const numberp1 = document.getElementById("number-p1");
+numberp1.textContent = '0';
+const numberp2 = document.getElementById("number-p2");
+numberp2.textContent = '0';
+const scorep1 = document.getElementById("score-p1");
+scorep1.textContent = '0';
+const scorep2 = document.getElementById("score-p2");
+scorep2.textContent = '0';
+
+}
+
+
+function RollDice(){
+
+  
+  const dice = document.getElementById("dice-pic");
+  const scorep1 = document.getElementById("score-p1"); //cumul des tirs de dé
+  const scorep2 = document.getElementById("score-p2"); //cumul des tirs de dé
+  const numberp1 = document.getElementById("number-p1"); //score validé
+  const numberp2 = document.getElementById("number-p2"); //score valide
+
+
+  const random=getRandomInt(1, 7);
+  if(player=="p1"){scorep1.textContent=parseInt(scorep1.textContent)+random;}
+  else{if(player=="p2"){scorep2.textContent=parseInt(scorep2.textContent)+random;}}
+ 
+  if(random==1){
+    document.getElementById("dice").src="assets/dice_1.png";
+
+
+    if(player=="p1"){
+      scorep1.textContent="0";
+      player="p2";
+      alert('Au tour du joueur 2 !');
+    }else{
+      scorep2.textContent="0";
+      player="p1";
+      alert('Au tout du Joueur 1 !')
+    }
+
+    }
+
+if(random==2){document.getElementById("dice").src="assets/dice_2.png";}
+if(random==3){document.getElementById("dice").src="assets/dice_3.png";}
+if(random==4){document.getElementById("dice").src="assets/dice_4.png";}
+if(random==5){document.getElementById("dice").src="assets/dice_5.png";}
+if(random==6){document.getElementById("dice").src="assets/dice_6.png";}
+
+}
+
+function Hold() {
+
+  const numberp1 = document.getElementById("number-p1"); //score validé
+  const numberp2 = document.getElementById("number-p2"); //score valide
+  const scorep1 = document.getElementById("score-p1"); //cumul des tirs de dé
+  const scorep2 = document.getElementById("score-p2"); //cumul des tirs de dé
+
+
+  if(player=="p1")
+  {
+  numberp1.textContent= parseInt(numberp1.textContent) + parseInt(scorep1.textContent);
+  scorep1.textContent="0";
+  player="p2";
+  if(parseInt(numberp1.textContent)>=100)
+  {
+
+    alert("Le joueur 1 a gagné !!!");
+    newGame();
+    
+  }else{
+
+  alert("Au tour du joueur 2 !");
+  newGame();
+  }
+
+  }else{
+
+  numberp2.textContent= parseInt(numberp2.textContent) + parseInt(scorep2.textContent);
+  scorep2.textContent="0";
+  player="p1";
+  if(parseInt(numberp2.textContent)>=100)
+  {
+    alert('Le joueur 2 a gagné !!!');
+  }else{
+
+  alert("Au tour du joueur 1 !");
+  }
+
+  }
+
+  
+ 
+ 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
---
+++ indextest.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Jeu Studi</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+
+ 
+
+  <div class="white-block">
+
+    <div class="red-block">
+      <p>CURRENT</p>
+     </div>
+
+  <div class="red-block">
+    <p>CURRENT</p>
+  </div>
+
+    
+
+    
+
+
+
+  </div>
+
+
+
+
+  </div>
+</div>
+
+<style>
+
+body{
+  background: rgb(170,152,119);
+background: -moz-linear-gradient(top, rgb(170,152,119) 0%, rgb(132,76,52) 100%);
+background: -webkit-linear-gradient(top, rgb(170,152,119) 0%, rgb(132,76,52) 100%);
+background: linear-gradient(to bottom, rgb(170,152,119) 0%, rgb(132,76,52) 100%);
+}
+  .container{
+    display:grid;
+    width:100%;
+    height:100vh;
+    background: rgb(170,152,119);
+background: -moz-linear-gradient(top, rgb(170,152,119) 0%, rgb(132,76,52) 100%);
+background: -webkit-linear-gradient(top, rgb(170,152,119) 0%, rgb(132,76,52) 100%);
+background: linear-gradient(to bottom, rgb(170,152,119) 0%, rgb(132,76,52) 100%);
+    align-items:center;
+    justify-content:center;
+  }
+
+
+  .white-block{
+    display:flex;
+    width:96%;
+    height:90vh;
+
+    align-items:flex-end;
+    justify-content:space-around;
+    background: linear-gradient(to right, #f8f4f4 50%, white 50%);
+  }
+
+  .de{
+    background-image: url("https://dobbelsteen.virtuworld.net/img/6c.gif");
+    display:flex;
+    width:150px;
+    height:150px;
+    align-items:center;
+    justify-content:center;
+  }
+
+    .red-block{
+    display:flex;
+    width:10%;
+    height:15vh;
+    background-color: #f04c4c;
+    align-items:center;
+    justify-content:center;
+    margin-bottom:10%;
+    font-family: 'Lato', sans-serif;
+
+  }
+
+
+
+
+
+  #conteneur
+{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+.joueur:nth-child(2){
+    background-color: blue;
+  }
+
+
+/* ... */
+
+</style>
+
+</body>
+</html>
+
+<style>
+
+</style>
